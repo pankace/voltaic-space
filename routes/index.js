ae2c792a930a1349699ec07daeb7fc42b2d29daa
pankace <44595774+pankace@users.noreply.github.com>
2021-07-26 02:21:16 +0700

    test click

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,3 +8,28 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+const port = "1111"
+const WebSocket = require("ws")
+const wss = new WebSocket.Server({ port })
+
+let count = 0
+
+function _onMessage(socket, message) {
+  console.log(message)
+  count += 1
+  wss.clients.forEach((client) => {
+    client.send(count)
+  })
+}
+
+wss.on("connection", (socket, request) => {
+  console.log("someone connect")
+
+  socket.send(count)
+  socket.on("message", (message) => _onMessage(socket, message))
+})
+
+wss.on("message", (socket, request) => {
+  console.log({ socket, request })
+})
